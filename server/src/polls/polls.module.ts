85c9dc1ae1d95763812fbef53af3bd34be078d95
WidedOueslati <ouesswided@gmail.com>
2024-05-04 19:35:20 +0100

    configured auth guard

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PollsController } from './polls.controller';
 import { PollsService } from './polls.service';
-import { redisModule } from '../redis/modules.config';
+import { jwtModule, redisModule } from '../redis/modules.config';
 import { PollsRepository } from './polls.repository';
 
 @Module({
-  imports: [ConfigModule, redisModule],
+  imports: [ConfigModule, redisModule, jwtModule],
   controllers: [PollsController],
   providers: [PollsService, PollsRepository],
 })
