c6044ddb42d88593504df9e2ed9214bfd31db6de
AlaBhs <alabelhadjsalem@gmail.com>
2024-05-06 15:29:44 +0100

    add catch the type exeption

--- server/src/exceptions/ws-catch-all-filter.ts
+++ server/src/exceptions/ws-catch-all-filter.ts
@@ -1,26 +1,37 @@
 import {
-    ArgumentsHost,
-    Catch,
-    ExceptionFilter,
-    BadRequestException,  } from '@nestjs/common';
-  import { SocketWithAuth } from 'src/polls/types';
-  import { WsBadRequestException, WsUnknownException } from './ws-exceptions';
-  
-  @Catch()
-  export class WsCatchAllFilter implements ExceptionFilter {
-    catch(exception: Error, host: ArgumentsHost) {
-      const socket: SocketWithAuth = host.switchToWs().getClient();
-  
-      if (exception instanceof BadRequestException) {
-        const exceptionData = exception.getResponse();
-        const exceptionMessage = exceptionData['message'] ?? exceptionData?? exception.name;
-  
-        const wsException = new WsBadRequestException(exceptionMessage);
-        socket.emit('exception', wsException.getError());
-        return;
-      }
-  
-      const wsException = new WsUnknownException(exception.message);
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  BadRequestException,
+} from '@nestjs/common';
+import { SocketWithAuth } from 'src/polls/types';
+import {
+  WsBadRequestException,
+  WsTypeException,
+  WsUnknownException,
+} from './ws-exceptions';
+
+@Catch()
+export class WsCatchAllFilter implements ExceptionFilter {
+  catch(exception: Error, host: ArgumentsHost) {
+    const socket: SocketWithAuth = host.switchToWs().getClient();
+
+    if (exception instanceof BadRequestException) {
+      const exceptionData = exception.getResponse();
+      const exceptionMessage =
+        exceptionData['message'] ?? exceptionData ?? exception.name;
+
+      const wsException = new WsBadRequestException(exceptionMessage);
       socket.emit('exception', wsException.getError());
+      return;
     }
+
+    if (exception instanceof WsTypeException) {
+      socket.emit('exception', exception.getError());
+      return;
+    }
+
+    const wsException = new WsUnknownException(exception.message);
+    socket.emit('exception', wsException.getError());
   }
+}
