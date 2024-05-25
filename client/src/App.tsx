96fde907c16812fdea0cd0dc0bda5ce1abd6c546
Nouira Taher <tahernouira003@gmail.com>
2024-05-25 15:15:41 +0200

    aamalt l page welcome , snaaet chwaya react components w aamalt transitions w settit l valtio for state management

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { devtools } from 'valtio/utils';
+import './index.css';
+import Pages from './Pages';
+import { state } from './state';
+
+devtools(state, 'app state');
+const App: React.FC = () => <Pages />;
+
+export default App;
