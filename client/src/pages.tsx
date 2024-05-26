d8f234ca436983ad6bed2fff5566d7c259a85e10
AlaBhs <alabelhadjsalem@gmail.com>
2024-05-26 17:30:26 +0100

    +Voting Page

--- client/src/pages.tsx
+++ client/src/pages.tsx
@@ -4,6 +4,7 @@ import { CSSTransition } from 'react-transition-group';
 import { useSnapshot } from 'valtio';
 import Create from './pages/Create';
 import Join from './pages/Join';
+import { Voting } from './pages/Voting';
 import { WaitingRoom } from './pages/WaitingRoom';
 import Welcome from './pages/Welcome';
 import { actions, AppPage, state } from './State';
@@ -13,6 +14,7 @@ const routeConfig = {
   [AppPage.Create]: Create,
   [AppPage.Join]: Join,
   [AppPage.WaitingRoom]: WaitingRoom,
+  [AppPage.Voting]: Voting,
 };
 
 const Pages: React.FC = () => {
@@ -25,6 +27,10 @@ const Pages: React.FC = () => {
       !currentState.poll?.hasStarted
     ) {
       actions.setPage(AppPage.WaitingRoom);
+    }
+
+    if (currentState.me?.id && currentState.poll?.hasStarted) {
+      actions.setPage(AppPage.Voting);
     }
 
     // add sequential check here
